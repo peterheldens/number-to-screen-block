25f2e94371ca4d620dd5b211f8219450d2059159
Peter Heldens <peter.heldens@live.nl>
2020-07-05 10:49:09 +0200

    first version

--- main.ts
+++ main.ts
@@ -1,5 +1,10 @@
 namespace screenMagic {
-    
+    /**
+     * Plot number on LEDs
+     */
+    //% blockId=screenMagicPlotAt
+    //% block="plt at $index"
+    //% index.min=0 index.max=25
     export function plotAt (index: number) {
         index |= 0
         const y = Math.floor(index / 5)
--- test.ts
+++ test.ts
@@ -1,1 +1,7 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+let x = 0
+let y = 0
+for (let i = 0; i <= 24; i++) {
+    plotAt(i)
+    pause(100)
+}
