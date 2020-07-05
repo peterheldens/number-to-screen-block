52ca263155501e8f924f3053cbb6f0d3bea8c241
Peter Heldens <peter.heldens@live.nl>
2020-07-05 16:45:18 +0200

    added plot Meter

--- main.ts
+++ main.ts
@@ -11,5 +11,21 @@ namespace screenMagic {
         const x = Math.floor(index % 5)
         led.plot(x, y)
     }
+    /**
+     * Plot meters on row 0, dm on row 2,3, cn on row 4,5
+     */
+    //% bl
+    //% blockId=screenMagicPlotMeter
+    //% block="plot at $number $row"
+    //% index.min=0 index.max=10
+    export function plotMeter (num: number, row: number) {
+        for (let col = 0; col <= 10; col++) {
+            if (col <= num) {
+                led.plot(col % 5, row + Math.idiv(col, 5))
+            } else {
+                led.unplot(col % 5, row + Math.idiv(col, 5))
+            }
+        }
+    }
 }
 
